f90f61b8f5944e8513549cddd6209c168d3d42a6
Alex S <habahabahaba@outlook.com>
2023-08-07 21:53:41 +0400

    functional Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,82 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
 
-export default class Search extends Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {
-    searchInput: '',
-    searchType: '',
-  };
-
-  handleSubmit(event) {
+export default function Search({ handleSearch }) {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchType, setSearchType] = useState('');
+  function handleSubmit(event) {
     event.preventDefault();
-    this.props.handleSearch(
-      this.state.searchInput.trim(),
-      this.state.searchType
-    );
-    // this.setState({ searchInput: '' });
+    handleSearch(searchInput.trim(), searchType);
   }
-  handleFilter(event) {
-    this.props.handleSearch(this.state.searchInput.trim(), event.target.value);
-    this.setState({ searchType: event.target.value });
+  function handleFilter(event) {
+    handleSearch(searchInput.trim(), event.target.value);
+    setSearchType(event.target.value);
   }
-  render() {
-    const { searchInput } = this.state;
-    return (
-      <div className='row'>
-        <form className='col s12' onSubmit={this.handleSubmit.bind(this)}>
-          <div className='input-field '>
+
+  return (
+    <div className='row'>
+      <form className='col s12' onSubmit={handleSubmit}>
+        <div className='input-field '>
+          <input
+            className='validate'
+            type='search'
+            placeholder='search...'
+            value={searchInput}
+            onChange={(ev) => setSearchInput(ev.target.value)}
+          />
+          <button type='submit' className='btn search-btn #795548 brown'>
+            Search
+          </button>
+        </div>
+        <br />
+        <div
+          className='radio'
+          // onChange={(event) => handleFilter}
+        >
+          <label>
             <input
-              className='validate'
-              type='search'
-              placeholder='search...'
-              value={searchInput}
-              onChange={(ev) => this.setState({ searchInput: ev.target.value })}
+              className='with-gap'
+              name='type'
+              type='radio'
+              value=''
+              defaultChecked
+              onChange={handleFilter}
             />
-            <button type='submit' className='btn search-btn #795548 brown'>
-              Search
-            </button>
-          </div>
-          <br />
-          <div
-            className='radio'
-            onChange={(event) => this.handleFilter.bind(this)}
-          >
-            <label>
-              <input
-                className='with-gap'
-                name='type'
-                type='radio'
-                value=''
-                defaultChecked
-                onChange={this.handleFilter.bind(this)}
-              />
-              <span>All</span>
-            </label>
-            <label>
-              <input
-                className='with-gap'
-                name='type'
-                type='radio'
-                value='movie'
-                onChange={this.handleFilter.bind(this)}
-              />
-              <span>Movies</span>
-            </label>
-            <label>
-              <input
-                className='with-gap'
-                name='type'
-                type='radio'
-                value='series'
-                onChange={this.handleFilter.bind(this)}
-              />
-              <span>Series</span>
-            </label>
-          </div>
-        </form>
-      </div>
-    );
-  }
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              value='movie'
+              onChange={handleFilter}
+            />
+            <span>Movies</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              value='series'
+              onChange={handleFilter}
+            />
+            <span>Series</span>
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
